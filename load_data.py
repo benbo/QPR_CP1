93cfb01130114b93a2fa2550da9a37372b726e44
Ben <ben@MacBook-Air-3.local>
2016-04-26 11:05:12 -0400

    initial commit of script to load data

---
+++ load_data.py
@@ -0,0 +1,41 @@
+import json
+import string
+
+#global variables
+charsWHITE =',.&!+:;?"#()\'*+,./<=>@[\\]^_`{|}~\n'
+remove_white_map = dict((ord(char), u' ') for char in charsWHITE)
+
+#functions
+
+def clean_text(t):
+    return t.translate(remove_white_map)
+
+def extract_data(data):
+    for i,d in enumerate(data):
+        try:
+            if 'extracted_text' in d['ad']:
+                text = d['ad']['extracted_text']
+            else:
+                text = d['ad']['extractions']['text']['results'][0]    
+            if d['class'] == 'positive':
+                yield clean_text(text),1,i,data[0]['ad']['_id']
+            else:
+                yield clean_text(text),0,i,data[0]['ad']['_id']
+        except:
+            print d
+
+def write_text(text,out = 'ht_training_text.txt'):
+    with open(out,'w') as f:
+        for line in text:
+            f.write(line.encode('utf-8'))
+            f.write('\n')
+#load data
+filename = 'ht_training.txt'
+with open(filename,'r') as f:
+    data =[json.loads(line) for line in f] 
+
+filename = 'ht_training.txt'
+with open(filename,'r') as f:
+    data = data + [json.loads(line) for line in f] 
+
+text,labels,indices,ad_id = zip(*[ d for d in extract_data(data)])
