7bbd28b62750f0b2f5b823a9e0560086d2b1e727
Matt Barnes <barnes.matt.j@gmail.com>
2016-04-26 15:03:29 -0400

    Started adding some documentation

--- learn_classifier.py
+++ learn_classifier.py
@@ -13,31 +13,50 @@ from load_data import load_files
 
 
 class ClassifierLearner(object):
-    def __init__(self,labels,text_dict):
-
+    def __init__(self, labels, text_dict):
         self.COUNT_BASE = {
-            'strip_accents':None, 'stop_words':'english', 'ngram_range':(1, 1), 'analyzer':'word', 'max_df':1.0, 'min_df':1, 'max_features':None, 'binary':False
+            'strip_accents': None,
+            'stop_words': 'english',
+            'ngram_range': (1, 1),
+            'analyzer': 'word',
+            'max_df': 1.0,
+            'min_df': 1,
+            'max_features': None,
+            'binary': False
             }
-
         self.remove_punct_map = dict((ord(char), u' ') for char in string.punctuation)
         self.remove_digit_map = dict((ord(char), u' ') for char in string.digits)
         self.printable = frozenset(string.printable)
-        self.skf = StratifiedKFold(labels, 5,random_state=17)
-        self.labels=labels
-        self.text=text_dict
-    def clean_text(self,t,rm_map):
-        return t.translate(rm_map)
+        self.skf = StratifiedKFold(labels, 5, random_state=17)
+        self.labels = labels
+        self.text = text_dict
 
-    def strip_non_ascii(self,t):
-        return filter(lambda x: x in self.printable, t) 
+    @staticmethod
+    def clean_text(text, rm_map):
+        """
+        :param text: String
+        :param rm_map: Dictionary of characters to replace [ordinal of character to replace, unicode character to replace with]
+        :return: Cleaned string
+        """
+        return text.translate(rm_map)
 
-    def get_features(self,data,options):
+    def strip_non_ascii(self, text):
+        return filter(lambda x: x in self.printable, text)
+
+    @staticmethod
+    def get_features(data, options):
         #if this operation is very expensive then we should store the results
-        vec = CountVectorizer(strip_accents=options['strip_accents'], stop_words=options['stop_words'], ngram_range=options['ngram_range'], analyzer=options['analyzer'],
-              max_df=options['max_df'], min_df=options['min_df'], max_features=options['max_features'], binary = options['binary'])
+        vec = CountVectorizer(strip_accents=options['strip_accents'], stop_words=options['stop_words'],
+                              ngram_range=options['ngram_range'], analyzer=options['analyzer'],
+                                max_df=options['max_df'], min_df=options['min_df'],
+                              max_features=options['max_features'], binary = options['binary'])
         return vec.fit_transform(data)
 
-    def call_experiment(self,args):
+    def call_experiment(self, args):
+        """
+        :param args: Hyperopt parameters
+        :return:
+        """
         #set up classifier
         model = args['model']    
         if model['model'] == 'LR':
@@ -53,9 +72,9 @@ class ClassifierLearner(object):
         text = self.text[args['text']['text']]#get raw text or ascii only text
 
         if args['cleaning']['strip_digits']:
-            text = [self.clean_text(t,self.remove_digit_map) for t in text]    
+            text = [self.clean_text(t, self.remove_digit_map) for t in text]
         if args['cleaning']['strip_punct']:
-            text = [self.clean_text(t,self.remove_punct_map) for t in text]    
+            text = [self.clean_text(t, self.remove_punct_map) for t in text]
 
      
         feat_options = args['features']
@@ -63,17 +82,17 @@ class ClassifierLearner(object):
         for key,value in self.COUNT_BASE.items():
             if key not in feat_options:
                 feat_options[key] = value
-        X = self.get_features(text,feat_options)
+        X = self.get_features(text, feat_options)
 
-        f1 = cross_val_score(model, X, self.labels, cv=self.skf,scoring='f1',n_jobs=8).mean()
+        f1 = cross_val_score(model, X, self.labels, cv=self.skf, scoring='f1', n_jobs=8).mean()
 
         print f1
         return {'loss': -f1, 'status': STATUS_OK}    
 
 if __name__ == '__main__':
-    raw_text,labels,indices,ad_ids,phones = load_files()
+    raw_text, labels, indices, ad_ids, phones = load_files()
     ascii_text = [strip_non_ascii(t) for t in raw_text]
-    CL = ClassifierLearner(labels,{'raw':raw_text,'ascii':ascii_text})
+    CL = ClassifierLearner(labels, {'raw': raw_text, 'ascii': ascii_text})
     trials = Trials()
     best = fmin(CL.call_experiment,
                 space=space,
