432c853e1febba5c787843c7c0bea6e15a259524
Benedikt Boecking <benediktb@lena.auton.cs.cmu.edu>
2016-04-27 10:24:01 -0400

    made learn_classifier object oriented. No comments yet.

--- learn_classifier.py
+++ learn_classifier.py
@@ -11,9 +11,8 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.feature_extraction.text import CountVectorizer
 from load_data import load_files
 
-
-class ClassifierLearner(object):
-    def __init__(self, labels, text_dict):
+class Featurizer(object):
+    def __init__(self,args=None):
         self.COUNT_BASE = {
             'strip_accents': None,
             'stop_words': 'english',
@@ -27,10 +26,23 @@ class ClassifierLearner(object):
         self.remove_punct_map = dict((ord(char), u' ') for char in string.punctuation)
         self.remove_digit_map = dict((ord(char), u' ') for char in string.digits)
         self.printable = frozenset(string.printable)
-        self.skf = StratifiedKFold(labels, 5, random_state=17)
-        self.labels = labels
-        self.text = text_dict
+        self.strip_digits=False
+        self.strip_punct=False
+        self.ascii_only = False
+        if not args is None:
+            feat_options = args['features']
+            for key,value in self.COUNT_BASE.items():
+                if key not in feat_options:
+                    feat_options[key] = value
+            self.options = feat_options
+            self.strip_digits=args['cleaning']['strip_digits']
+            self.strip_punct=args['cleaning']['strip_punct']
+        else:
+            self.options = self.COUNT_BASE
 
+    def strip_non_ascii(self, text):
+        return filter(lambda x: x in self.printable, text)
+    
     @staticmethod
     def clean_text(text, rm_map):
         """
@@ -39,10 +51,7 @@ class ClassifierLearner(object):
         :return: Cleaned string
         """
         return text.translate(rm_map)
-
-    def strip_non_ascii(self, text):
-        return filter(lambda x: x in self.printable, text)
-
+    
     @staticmethod
     def get_features(data, options):
         #if this operation is very expensive then we should store the results
@@ -52,54 +61,98 @@ class ClassifierLearner(object):
                               max_features=options['max_features'], binary = options['binary'])
         return vec.fit_transform(data)
 
-    def call_experiment(self, args):
-        """
-        :param args: Hyperopt parameters
-        :return:
-        """
-        #set up classifier
-        model = args['model']    
-        if model['model'] == 'LR':
-            model = lr(penalty=model['regularizer_lr'], C=model['C_lr'])
-        elif model['model'] == 'SVM':
-            if model['regularizer_svm'] == 'l1':
-                #squared hinge loss not available when penalty is l1. 
-                model = svm.LinearSVC(C=model['C_svm'], penalty=model['regularizer_svm'],dual=False)#loss='hinge')
-            else:
-                model = svm.LinearSVC(C=model['C_svm'], penalty=model['regularizer_svm'])
-      
-        #walk through text cleaning options
-        text = self.text[args['text']['text']]#get raw text or ascii only text
-
-        if args['cleaning']['strip_digits']:
-            text = [self.clean_text(t, self.remove_digit_map) for t in text]
-        if args['cleaning']['strip_punct']:
-            text = [self.clean_text(t, self.remove_punct_map) for t in text]
-
-     
+    def set_options(self,args):
         feat_options = args['features']
-        #set up features
         for key,value in self.COUNT_BASE.items():
             if key not in feat_options:
                 feat_options[key] = value
-        X = self.get_features(text, feat_options)
+        self.options = feat_options
+        self.strip_digits=args['cleaning']['strip_digits']
+        self.strip_punct=args['cleaning']['strip_punct']
+
+    def set_ascii(self, value):
+        if value:
+            self.ascii_only = True
+
+    def run(self,text):
+        #walk through text cleaning options
+        if self.ascii_only:
+            text = [self.strip_non_ascii(t) for t in text]
+        if self.strip_digits:
+            text = [self.clean_text(t, self.remove_digit_map) for t in text]
+        if self.strip_punct:
+            text = [self.clean_text(t, self.remove_punct_map) for t in text]
+
+        return self.get_features(text,self.options)
+
+
+
+class ClassifierLearner(object):
+    def __init__(self, label_dict, text_dict, num_folds=5):
+        #process and prepare text
+        self.Featurizer = Featurizer()
+        keys = text_dict.keys()
+        raw_text = [text_dict[k] for k in keys]
+        non_ascii = [self.Featurizer.strip_non_ascii(t) for t in raw_text]
+        self.text = {'raw':raw_text,'ascii':non_ascii}
+        #store labels
+        self.labels = [label_dict[k] for k in keys]
+        #set up CV
+        self.skf = StratifiedKFold(self.labels, num_folds, random_state=137)
+
+    def call_experiment(self, args):
+        """
+        :param args: Hyperopt parameters
+        :return: Hyperopt feedback
+        """
+        #set up classifier
+        model = self.get_model(args)   
+
+        #get raw or non-ascii text (removing non-ascii is expensive)      
+        text = self.text[args['text']['text']]#get raw text or ascii only text
+
+        self.Featurizer.set_options(args) 
+
+        X = self.Featurizer.run(text)
 
         f1 = cross_val_score(model, X, self.labels, cv=self.skf, scoring='f1', n_jobs=8).mean()
 
         print f1
         return {'loss': -f1, 'status': STATUS_OK}    
 
+    @staticmethod
+    def get_model(args):
+        if args['model']['model'] == 'LR':
+            model = lr(penalty=args['model']['regularizer_lr'], C=args['model']['C_lr'])
+        elif args['model']['model'] == 'SVM':
+            if args['model']['regularizer_svm'] == 'l1':
+                #squared hinge loss not available when penalty is l1. 
+                model = svm.LinearSVC(C=args['model']['C_svm'], penalty=args['model']['regularizer_svm'],dual=False)#loss='hinge')
+            else:
+                model = svm.LinearSVC(C=args['model']['C_svm'], penalty=args['model']['regularizer_svm'])
+        return model
+
+    def run(self,max_evals=100):
+        trials = Trials()
+        best = fmin(self.call_experiment,
+                    space=space,
+                    algo=tpe.suggest,
+                    max_evals=max_evals,
+                    trials=trials)
+        #print best
+        args = space_eval(space, best)
+        #print "losses:", [-l for l in trials.losses()]
+        #print max([-l for l in trials.losses()])
+        
+        #TODO
+        #return model and featurizer. Don't forget to set ascii option.
+        featurizer = Featurizer(args)
+        if args['text']['text'] == 'ascii':
+            featurizer.set_ascii(True)
+        return self.get_model(args),featurizer 
+
 if __name__ == '__main__':
-    raw_text, labels, indices, ad_ids, phones = load_files()
-    ascii_text = [strip_non_ascii(t) for t in raw_text]
-    CL = ClassifierLearner(labels, {'raw': raw_text, 'ascii': ascii_text})
-    trials = Trials()
-    best = fmin(CL.call_experiment,
-                space=space,
-                algo=tpe.suggest,
-                max_evals=100,
-                trials=trials)
-    print best
-    print space_eval(space, best)
-    print "losses:", [-l for l in trials.losses()]
-    print max([-l for l in trials.losses()])
+    #text_dict,label_dict,phone_dict = load_files(['data/ht_training_UPDATED.gz'])
+    text_dict,label_dict,phone_dict = load_files()
+    CL = ClassifierLearner(label_dict, text_dict, num_folds=5)
+    CL.run(max_evals=5)
